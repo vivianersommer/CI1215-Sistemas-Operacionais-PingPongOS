04b25a52f41be3a3b06a97c6613998109f7eb9c2
Viviane da Rosa Sommer <vivianersommer@gmail.com>
2020-08-02 17:44:01 -0300

    Adicionado arquivos base para P0

---
+++ queue.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "queue.h"
+
+/*
+    Implementar essas funções:
+    queue_append - adicionar elemento
+    queue_remove - remover elemento
+    queue_size - retorna o tamanho da fila
+    queue_print - imprime a fila
+*/

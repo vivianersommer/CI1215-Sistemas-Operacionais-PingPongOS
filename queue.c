ff3f5d72b696829ae1d88afbec5696b551de6d5e
Viviane <vivianersommer@gmail.com>
2020-08-02 22:55:51 -0300

    Teste - git

--- queue.c
+++ queue.c
@@ -4,9 +4,13 @@
 #include "queue.h"
 
 /*
+
+
     Implementar essas funções:
     queue_append - adicionar elemento
     queue_remove - remover elemento
     queue_size - retorna o tamanho da fila
     queue_print - imprime a fila
+
+
 */

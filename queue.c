bbae3b11aeba68fa202a92ac26e3fe967c94df9d
Viviane <vivianersommer@gmail.com>
2020-08-04 19:04:41 -0300

    Função add

--- queue.c
+++ queue.c
@@ -5,12 +5,40 @@
 
 /*
 
-
     Implementar essas funções:
     queue_append - adicionar elemento
     queue_remove - remover elemento
     queue_size - retorna o tamanho da fila
     queue_print - imprime a fila
 
-
 */
+
+void queue_append(queue_t **queue, queue_t *elem){
+
+    if(queue == NULL){
+		queue=elem;
+        elem->next = elem;
+        elem->prev = elem;
+    }
+    else{
+        queue_t *primeiro = *queue;
+        queue_t *ultimo = primeiro->prev;
+        ultimo->next = elem;
+        elem->prev = ultimo;
+        elem->next = primeiro;
+        primeiro->prev = elem;
+     }
+     
+}
+
+queue_t *queue_remove (queue_t **queue, queue_t *elem){
+
+}
+
+int queue_size(queue_t *queue){
+
+}
+
+void queue_print(char *name, queue_t *queue, void print_elem (void*) ){
+
+}

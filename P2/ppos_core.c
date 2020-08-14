661dc1a35110624b3a89ac5768483c0ea558484f
Viviane <vivianersommer@gmail.com>
2020-08-14 20:31:53 -0300

    Adicionado esqueleto

--- P2/ppos_core.c
+++ P2/ppos_core.c
@@ -18,3 +18,26 @@
 
 */
 
+void ppos_init (){
+
+    /* desativa o buffer da saida padrao (stdout), usado pela função printf */
+    setvbuf (stdout, 0, _IONBF, 0) ;
+
+}
+
+int task_create (task_t *task, void (*start_routine)(void *),  void *arg) {
+
+}
+
+int task_switch (task_t *task){
+
+}
+
+void task_exit (int exit_code){
+
+}
+
+int task_id (){
+    
+}
+

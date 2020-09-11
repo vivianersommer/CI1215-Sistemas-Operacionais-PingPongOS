b115115f9d3dbe929f8fce7bce9077949eb10b12
Millena <lmists13@gmail.com>
2020-09-10 21:18:54 -0300

    P10

---
+++ P10/ppos_core.c
@@ -0,0 +1,459 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <signal.h>
+#include <sys/time.h>
+#include "ppos.h"
+#include "ppos_data.h"
+
+#define DEBUG
+#define STACKSIZE 32768	
+#define N 100
+
+// estrutura que define um tratador de sinal (deve ser global ou static)
+struct sigaction action ;
+
+// estrutura de inicialização to timer
+struct itimerval timer ;
+struct itimerval ZeraTimer = {0} ;
+
+char *stack ;
+int i=0 , premp;
+int quantum ; 
+unsigned int relogio;
+task_t ContextMain, *ContextAtual ,*tarefasUser, Dispatcher , *tarefasNanando;
+
+/*  P6
+
+    Alunas: 
+    
+        GRR20182564 Viviane da Rosa Sommer
+        GRR20185174 Luzia Millena Santos Silva
+
+    Arquivo ppos_core.c:
+
+        Implementada as seguintes funções:
+            1 -  ppos_init  - Inicializa o sistema
+            2 -  task_create - Cria uma nova tarefa
+            3 -  task_switch  - Transfere o processador para outra tarefa
+            4 -  task_exit - Termina a tarefa corrente
+            5 -  task_id - Informa o identificador da tarefa corrente
+            6 -  task_yield - Faz a troca de contexto para o dispatcher
+            7 -  task_setprio - Ajusta a prioridade estática
+            8 -  task_getprio - Recebe a prioridade estática
+            9 -  scheduler  - Escolhe qual a próxima tarefa a ser executada
+            10 - dispatcher - Realiza a troca de contexto entre tarefas **(ela também é uma tarefa)
+            11 - tratador - Analiza se o quantum já acabou, e jaz a preempção
+            12 - temporizador - Ajusta o temporizador para fazer a preempção
+            13 - systime - Função que retorna uma simulação de relógio
+            14 - imprime_fila - Imprime a fila de forma organizada
+	    15 - task_join - Possibilita a sincronização de tarefas
+ 
+    Arquivo ppos_data.h:
+
+        Adicionado na estrutura task_t os seguintes campos:
+            1 -  status : estado da tarefa
+            2 -  prioridadeEstatica : nível de prioridade estatica da tarefa
+            3 -  prioridadeDinamica : nível de prioridade dinamica da tarefa 
+            4 -  tipoTarefa :  0 = tarefa de sistema e 1 = tarefa de usuário
+            5 -  horarioInicio : salva a hora q inicia a tarefa
+            6 -  horarioFim : salva a hora q finaliza a tarefa 
+            7 -  horarioProcessador : salva o tempo de execução da tarefa
+            8 -  ativacoes : conta quantas vezes a tarefa trocou de contexto
+
+        Adicionado as funções:
+            1 -  void dispatcher () ;
+            2 -  void temporizador() ;
+            3 -  void imprime_fila(task_t *fila);
+	    4 -  int task_join();
+*/
+
+void ppos_init (){
+
+    /* desativa o buffer da saida padrao (stdout), usado pela função printf */
+    setvbuf (stdout, 0, _IONBF, 0) ;
+    premp = 1;
+    tarefasUser = NULL;
+    tarefasNanando = NULL;
+    char *stack;
+    stack = malloc (STACKSIZE);
+    
+    if (stack){
+        ContextMain.context.uc_stack.ss_sp = stack;
+        ContextMain.context.uc_stack.ss_size = STACKSIZE;
+        ContextMain.context.uc_stack.ss_flags = 0;
+        ContextMain.context.uc_link = 0;
+        ContextMain.id = 0;
+        ContextMain.status = 0;
+        ContextMain.tipoTarefa = 1; //tarefa de usuário
+        ContextMain.horarioInicio = systime();
+        ContextMain.ativacoes = 0;
+        ContextMain.horarioProcessador = 0;
+        ContextMain.tarefasSuspensas = NULL;
+    }
+    else{
+        perror ("Erro na criação da pilha: ");
+        exit (-1);
+    }
+    task_setprio(&ContextMain,0);
+
+    ContextMain.next = NULL;
+    ContextMain.prev = NULL;
+
+    ContextAtual = &ContextMain;
+
+    Dispatcher.status = 1;
+    Dispatcher.tipoTarefa = 0; // tarefa de sistema
+
+    //Cria tarefa Main
+    task_create(&ContextMain, NULL , 0);
+
+    //Cria tarefa dispatcher
+    task_create(&Dispatcher, dispatcher, NULL);
+    queue_remove ((queue_t**) &tarefasUser, (queue_t*) &Dispatcher) ;
+    
+    // ajusta e ativa mecanismo de preempção por tempo
+    temporizador();
+
+    //Ativa dispatcher ao iniciar
+    task_yield () ;
+}
+
+int task_create (task_t *task, void (*start_routine)(void *),  void *arg) {
+
+    getcontext (&task->context) ;
+    char *stack; 
+    stack = malloc (STACKSIZE) ;
+
+    if (stack)
+    {
+        (&task->context)->uc_stack.ss_sp = stack ;
+        (&task->context)->uc_stack.ss_size = STACKSIZE ;
+        (&task->context)->uc_stack.ss_flags = 0 ;
+        (&task->context)->uc_link = 0 ;
+        *(&task->id) = i++;
+        task->status = 0;
+        task->horarioInicio = systime();
+        task->ativacoes = 0 ;
+        task->horarioProcessador = 0;
+        task->tarefasSuspensas = NULL;
+        task->horaAcordar = 0;
+        if(task->id != 1){
+            task->tipoTarefa = 1; //tarefa de usuário
+        }
+    }
+    else
+    {
+        perror ("Erro na criação da pilha: ") ;
+        return (-1) ;
+    }
+
+    queue_t* context = (queue_t*) task;
+    context->next = NULL;
+    context->prev = NULL;
+    task_setprio(task,0);
+
+    makecontext (&task->context, (void*)(*start_routine), 1, arg);
+
+    queue_append ((queue_t **) &tarefasUser,  context) ;
+
+    if(task->id == 0){
+        #ifdef DEBUG
+        printf ("task_create: criou tarefa %d - MAIN\n", task->id) ;
+        #endif
+    }else if(task->id == 1){
+        #ifdef DEBUG
+        printf ("task_create: criou tarefa %d - DISPACHER\n", task->id) ;
+        #endif
+    }
+    else{
+        #ifdef DEBUG
+        printf ("task_create: criou tarefa %d\n", task->id) ;
+        #endif
+    }
+
+    return task_id();     
+}
+
+int task_switch (task_t *task){
+
+    task_t *ContextoAntigo;
+    ContextoAntigo = ContextAtual;
+    ContextAtual = task;
+    if(ContextoAntigo->status != 2){
+        ContextoAntigo->status = 1;
+    }
+    ContextAtual->status = 0;
+
+    // #ifdef DEBUG
+    // printf ("task_switch: trocando contexto %d para %d\n",ContextoAntigo->id, task->id) ;
+    // #endif
+
+    swapcontext (&ContextoAntigo->context,&task->context) ;
+    return task_id(); 
+}
+
+void task_exit (int exit_code){
+
+    task_t *aux = ContextAtual->tarefasSuspensas;
+    if(aux != NULL){
+        task_t *trocar = aux;
+        aux->status = 0;
+        aux = aux->next;    
+        queue_remove ( ( queue_t** ) &(ContextAtual->tarefasSuspensas), (queue_t*) trocar) ; 
+        trocar->next = NULL;
+        trocar->prev = NULL;
+        queue_append ( ( queue_t** ) &tarefasUser, (queue_t*) trocar ) ;
+    }
+
+    ContextAtual->status = 2;
+    ContextAtual->exitcode = exit_code;
+    ContextAtual->horarioFim = systime();
+
+    
+    #ifdef DEBUG
+    printf ("Task %d exit: execution time %d ms, processor time %d ms, %d activations\n", 
+            ContextAtual->id, (ContextAtual->horarioFim - ContextAtual->horarioInicio),
+            ContextAtual->horarioProcessador , ContextAtual->ativacoes) ;
+    #endif
+
+    if(ContextAtual->id == 1){
+        task_switch(&ContextMain);
+    }else{
+        task_switch(&Dispatcher);
+    }
+    
+}
+
+int task_id (){
+    return (int) *(&ContextAtual->id);
+}
+
+void task_yield(){  //Realiza a troca de contexto para o dispatcher
+    if(ContextAtual->id != 0){
+        ContextAtual->status = 0;
+        queue_append((queue_t**)&tarefasUser, (queue_t*)ContextAtual);
+    }
+    task_switch(&Dispatcher);
+}
+
+void task_setprio (task_t *task, int prio){
+    if(task && prio > -20 && prio < 20 ){ // Analiza se o tarefa existe, e se o valor passado está dentro dos limites 
+        task->prioridadeEstatica = prio;
+        task->prioridadeDinamica = prio;
+    }
+    else{
+        perror ("Erro ao setar a prioridade da tarefa: ");
+        exit (-1);
+    }
+}
+
+int task_getprio (task_t *task){
+    if(task == NULL){  //se não for enviada uma tarefa, retorna o valor da prioridade do Contexto Atual
+        return (int) *(&ContextAtual->prioridadeEstatica);
+    }
+    return task->prioridadeEstatica;
+}
+
+task_t *scheduler(task_t *tarefasUser){
+   task_t *prox = tarefasUser;
+   task_t *aux = tarefasUser->next;
+
+   while(aux != tarefasUser){
+      if(aux->prioridadeDinamica <= prox->prioridadeDinamica){
+         prox = aux; //coloca a prox como a que tem a maior prioridade dinamica
+      }
+      aux = aux->next;
+   }
+   
+   aux = tarefasUser;
+   while(tarefasUser->next != aux){
+      if(tarefasUser->id != prox->id){
+         tarefasUser->prioridadeDinamica = tarefasUser->prioridadeDinamica - 1; //envelhece a não prioritaria
+      }
+      tarefasUser = tarefasUser->next;
+   }
+
+   prox->prioridadeDinamica = prox->prioridadeEstatica; //a prioridade dinamica volta ao valor original da prioridade estatica
+	return prox;
+}
+
+void dispatcher () {   
+    while( queue_size( (queue_t*)tarefasUser) > 0 || queue_size( (queue_t*)tarefasNanando) > 0) { //analiza se existe algum elemento na fila de tarefas prontas
+        if(tarefasUser != NULL){
+            task_t *prox = scheduler(tarefasUser);
+            if(prox != NULL){ //se o scheduler retorna uma tarefa, retira ela da fila e realiza task_switch
+                quantum = 20;
+                prox->ativacoes = prox->ativacoes + 1;
+                int processadorInicio = systime();
+                queue_remove ((queue_t**) &tarefasUser, (queue_t*) prox) ;
+                task_switch (prox);
+                Dispatcher.ativacoes = Dispatcher.ativacoes + 1;
+                prox->horarioProcessador = prox->horarioProcessador + (systime() - processadorInicio);
+            }
+        }
+        acordaTarefas();
+
+   }
+   task_exit(0);  //quando a fila esvazia, encerra o dispatcher, pois ele também é uma tarefa
+}
+
+void tratador (int signum)
+{
+    relogio ++;
+    if(ContextAtual->tipoTarefa == 1){ //apenas faz preempção quando é tarefa de usuário
+        quantum --;
+        if(quantum == 0 && premp == 1){ //troca de contexto quando acaba o quantum
+            premp = 0;
+            queue_append((queue_t**)&tarefasUser, (queue_t*)ContextAtual);
+            premp = 1;
+            task_switch(&Dispatcher);
+        }
+    }
+}
+
+void temporizador(){
+    action.sa_handler = tratador ;
+    sigemptyset (&action.sa_mask) ;
+    action.sa_flags = 0 ;
+    if (sigaction (SIGALRM, &action, 0) < 0)
+    {
+        perror ("Erro em sigaction: ") ;
+        exit (1) ;
+    }
+
+    // ajusta valores do temporizador
+    timer.it_value.tv_usec = 1000 ;      // primeiro disparo, em micro-segundos
+    timer.it_value.tv_sec  = 0 ;      // primeiro disparo, em segundos
+    timer.it_interval.tv_usec = 1000 ;   // disparos subsequentes, em micro-segundos
+    timer.it_interval.tv_sec  = 0 ;   // disparos subsequentes, em segundos
+
+    // arma o temporizador ITIMER_REAL (vide man setitimer)
+    if (setitimer (ITIMER_REAL, &timer, 0) < 0)
+    {
+        perror ("Erro em setitimer: ") ;
+        exit (1) ;
+    }
+}
+
+unsigned int systime () { //cálculo de tempo
+    return relogio;
+}
+
+
+void imprime_fila(task_t *tarefasUser){ // função extra para imprimir o conteudo da fila
+
+	printf("Saída gerada: ");
+	if (tarefasUser == NULL ) {
+		printf("[] \n");
+		return;	
+	}
+	printf("[");
+	task_t *aux = tarefasUser;
+	printf(" %d ",aux->id);
+	printf(" ");
+	aux = aux->next;
+	while (aux != tarefasUser){
+	printf(" %d ",aux->id);
+		aux = aux->next;
+		printf(" ");
+	} 
+	printf("] \n");
+	return;		
+}
+
+
+int task_join(task_t *task){
+
+    premp = 0;
+	
+	if ( task == NULL ){
+		return -1;
+	}
+
+    if(task->status == 2){
+        return task->exitcode;
+    }
+
+    //suspende tarefa atual 
+    ContextAtual->status = 1;
+    queue_remove ( ( queue_t** ) &tarefasUser, (queue_t*) ContextAtual ) ;
+    queue_t* context = (queue_t*) ContextAtual;
+    if(ContextAtual!=NULL){
+	    context->next = NULL;
+	    context->prev = NULL;
+    }
+
+    queue_append ( ( queue_t** ) &task->tarefasSuspensas , ( context )) ;
+
+    premp = 1;
+
+    task_yield();
+
+    return task->exitcode;
+}
+
+void task_sleep (int t){
+    if(ContextAtual == NULL){
+        return;
+    }
+
+    premp = 0;
+
+    task_t *trocar = ContextAtual;
+    ContextAtual->horaAcordar = systime() + t;
+    ContextAtual->status = 1;
+    queue_remove ( ( queue_t** ) &tarefasUser, (queue_t*) trocar) ; 
+    trocar->next = NULL;
+    trocar->prev = NULL;
+    queue_append ( ( queue_t** ) &tarefasNanando, (queue_t*) trocar ) ;
+
+    premp = 1;
+    
+    task_yield();
+}
+
+void acordaTarefas(){
+    if(tarefasNanando == NULL){
+        return;
+    }
+    premp = 0;
+    task_t *aux = tarefasNanando;
+    do{
+        if(aux->horaAcordar <= systime()){
+            task_t *trocar = aux;
+            aux->status = 0;
+            aux = aux->next;    
+            queue_remove ( ( queue_t** ) &tarefasNanando, (queue_t*) trocar) ; 
+            trocar->next = NULL;
+            trocar->prev = NULL;
+            queue_append ( ( queue_t** ) &tarefasUser, (queue_t*) trocar ) ;
+        }
+        else{
+            aux = aux->next;    
+        }
+    } while((tarefasNanando!= NULL && aux!= NULL) && tarefasNanando != aux );
+    premp = 1;
+    return;
+}
+
+
+int sem_create (semaphore_t *s, int value){
+
+
+}
+
+
+int sem_down (semaphore_t *s){
+
+}
+
+
+int sem_up (semaphore_t *s){
+
+}
+
+
+int sem_destroy (semaphore_t *s){
+
+}
---
+++ P10/ppos_data.h
@@ -0,0 +1,93 @@
+// PingPongOS - PingPong Operating System
+// Prof. Carlos A. Maziero, DINF UFPR
+// Versão 1.1 -- Julho de 2016
+
+// Estruturas de dados internas do sistema operacional
+
+#ifndef __PPOS_DATA__
+#define __PPOS_DATA__
+
+#include <ucontext.h>		// biblioteca POSIX de trocas de contexto
+#include "queue.h"		// biblioteca de filas genéricas
+
+// Estrutura que define um Task Control Block (TCB)
+
+typedef enum Status
+{
+  Pronta, //0
+  Suspensa,  //1
+  Terminada,  //2
+  Dormindo //3
+} Status; 
+
+typedef struct task_t
+{
+   struct task_t *prev, *next ;		// ponteiros para usar em filas
+   int id ;				// identificador da tarefa
+   ucontext_t context ;			// contexto armazenado da tarefa
+   Status status; //estado da tarefa
+   int prioridadeEstatica; // nível de prioridade estatica da tarefa 
+   int prioridadeDinamica; // nível de prioridade dinamica da tarefa 
+   int tipoTarefa; // 0 : tarefa de sistema e 1 : tarefa de usuário
+   int horarioInicio; // salva a hora q inicia a tarefa
+   int horarioFim; // salva a hora q finaliza a tarefa
+   int horarioProcessador; // salva o tempo de execução
+   int ativacoes; //quantas vezes trocou de contexto
+   struct task_t *tarefasSuspensas;  //tarefas que foram suspensas por task_join()
+   int horaAcordar;  //hora que a tarefa acorda (SE MANCA TAREFA)
+   int exitcode; 
+   // ... (outros campos serão adicionados mais tarde)
+} task_t ;
+
+
+// funções auxiliares usadas no ppos_core.c
+void dispatcher () ;
+//int main (int argc, char *argv[]);
+void temporizador() ;
+
+void imprime_fila(task_t *fila);
+
+void acordaTarefas();
+
+int main (int argc, char *argv[]);
+
+
+// estrutura que define um semáforo
+typedef struct
+{
+  struct task_t *tarefasSuspensas;
+  int counter;
+  // preencher quando necessário
+} semaphore_t ;
+
+
+int sem_create (semaphore_t *s, int value);
+
+int sem_down (semaphore_t *s);
+
+int sem_up (semaphore_t *s);
+
+int sem_destroy (semaphore_t *s);
+
+
+// estrutura que define um mutex
+typedef struct
+{
+  // preencher quando necessário
+} mutex_t ;
+
+// estrutura que define uma barreira
+typedef struct
+{
+  // preencher quando necessário
+} barrier_t ;
+
+
+
+// estrutura que define uma fila de mensagens
+typedef struct
+{
+  // preencher quando necessário
+} mqueue_t ;
+
+#endif

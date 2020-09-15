bc20c2d4e6d2383831627b30b10fccf12aea04d2
Millena <lmists13@gmail.com>
2020-09-15 20:29:09 -0300

    inicio P11

---
+++ P11/pingpong-prodcons.c
@@ -0,0 +1,47 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "ppos.h"
+
+#define item 99
+
+
+void produtor() {
+	while (true)
+	{
+		task_sleep (1000)
+			item = random (0..99)
+
+			down (s_vaga)
+
+			down (s_buffer)
+			insere item no buffer
+			up (s_buffer)
+
+			up (s_item)
+	}
+}
+
+void consumidor() {
+	while (true)
+	{
+		down (s_item)
+
+			down (s_buffer)
+			retira item do buffer
+			up (s_buffer)
+
+			up (s_vaga)
+
+			print item
+			task_sleep (1000)
+	}
+}
+
+int main () {
+
+
+	task_create(consumidor);
+	task_create(produtor);
+
+}

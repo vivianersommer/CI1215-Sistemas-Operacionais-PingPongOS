87c668cbc145768a57dd8dc0678f8f287db0bea4
Viviane <vivianersommer@gmail.com>
2020-09-15 22:08:53 -0300

    Arrumado para vetor fifo circular

--- P11/pingpong-prodcons.c
+++ P11/pingpong-prodcons.c
@@ -8,20 +8,23 @@ task_t      p1, p2, p3, c1, c2;
 semaphore_t s_buffer, s_item, s_vaga ;
 int item = 0;
 int buffer[4];
-int pos = 0;
+int primeiro = 0;
+int ultimo = -1;
+int capacidade = 5;
 
 void produtor(void * arg) {
 	while (1)
 	{
-		if(pos == 4){
-			pos = 0;
+		if(ultimo == (capacidade-1)){ 
+			ultimo = -1;
 		}
 		task_sleep (1000);
 		item =  (rand() % 100);
 		sem_down(&s_vaga);
 		sem_down(&s_buffer);
-		buffer[pos + 1] = item;
-		printf ("%s produziu (%d)\n", (char *) arg, item) ;
+		ultimo++;
+		buffer[ultimo] = item;
+		printf ("%s produziu (%d) na posição %d \n", (char *) arg, item,ultimo) ;
 		sem_up (&s_buffer);
 		sem_up (&s_item);
 	}
@@ -30,15 +33,15 @@ void produtor(void * arg) {
 void consumidor(void * arg) {
 	while (1)
 	{
-		if(pos == 4){
-			pos = 0;
-		}
 		sem_down (&s_item);
 		sem_down (&s_buffer);
 		sem_up (&s_buffer);
 		sem_up (&s_vaga);
-		printf ("%s consumiu (%d)\n", (char *) arg, buffer[pos + 1]) ;
-		pos ++;
+		int teste = primeiro++;
+		printf ("%s consumiu (%d) na posição %d \n", (char *) arg, buffer[teste], teste) ;
+		if(primeiro == capacidade){
+			primeiro = 0;
+		}
 		task_sleep (1000);
 	}
 }
@@ -55,23 +58,14 @@ int main (int argc, char *argv[]) {
 
     task_create (&p1, produtor, " p1 ") ;
     task_create (&p2, produtor, " p2 ") ;
-    task_create (&p3, consumidor, " p3 ") ;
+    task_create (&p3, produtor, " p3 ") ;
     task_create (&c1, consumidor, "                   c1 ") ;
     task_create (&c2, consumidor, "                   c2 ") ;
-
-	task_join (&p1) ;
 
     // destroi semaforos
     sem_destroy (&s_buffer) ;
     sem_destroy (&s_item) ;
 	sem_destroy (&s_vaga) ;
-
-
-	// aguarda a2, b1 e b2 encerrarem
-	task_join (&c1) ;
-	task_join (&p2) ;
-	task_join (&c2) ;
-	task_join (&p3) ;
 
 	printf ("main: fim\n") ;
 	task_exit (0) ;
